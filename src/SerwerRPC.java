99a72da7279d20f6c6a158b70a6588d160559083
michalinajnk <254564@student.pwr.edu.pl>
2022-03-11 23:40:07 +0100

    Server files added

---
+++ src/SerwerRPC.java
@@ -0,0 +1,42 @@
+
+import org.apache.xmlrpc.WebServer;
+
+public class SerwerRPC {
+    public static void main(String [] args){
+       try{
+           System.out.println("Startuje serwer XML-RPC...");
+           int port= 10005;
+           WebServer server= new WebServer(port);
+
+           server.addHandler("MojSerwer", new SerwerRPC());
+           server.start();
+           System.out.println("Serwer wystartowal pomyslnie.");
+           System.out.println("Nasluchuje na porcie: " + port);
+           System.out.println("Aby zatrzymac serwer nacisnij ctrl+c");
+
+
+
+       } catch(Exception exception){
+           System.err.println("Serwer XML-RPC: " + exception);
+       }
+
+    }
+
+    public int execAsy(int x){
+        System.out.println("...wywolano asy -odliczam");
+        try{
+            Thread.sleep(x);
+        } catch(InterruptedException e){
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("...asy - koniec odliczania");
+        return(123);
+
+    }
+    public Integer echo(int x, int y){
+        return new Integer(x+y);
+    }
+
+
+}
