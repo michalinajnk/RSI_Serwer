125eec3a1a4ff7c27fe4e40afa082729f517ed96
MrTitainin <jaromirb@op.pl>
2022-03-21 05:58:26 +0100

    Server methods

--- src/SerwerRPC.java
+++ src/SerwerRPC.java
@@ -2,6 +2,8 @@
 import org.apache.xmlrpc.WebServer;
 
 public class SerwerRPC {
+    private static int EARTH_RADIUS = 6371;
+
     public static void main(String [] args){
        try{
            MyData.info();
@@ -23,6 +25,47 @@ public class SerwerRPC {
 
     }
 
+    public int[] myPrimes(int min, int max){
+        max++;
+        boolean[] bool = new boolean[max+1];
+        int count = min>2?0:1;
+        bool[2] = true;
+        for(int ii = 3; ii <= max; ii+=2) {
+            bool[ii] = true;
+        }
+        for(int ii = 3; ii <= max; ii+=2) {
+            if(bool[ii]) {
+                if(ii >= min) count++;
+                for(int jj = ii*ii; jj <= max; jj += ii) {
+                    bool[jj] = false;
+                }
+            }
+        }
+        int highest = 0;
+        for (int ii = max;highest==0 && ii>=min;ii--){
+            if(bool[ii]){
+                highest = ii;
+                break;
+            }
+        }
+        return new int[]{count,highest};
+    }
+
+    public double distance(double lat1, double lon1, double lat2, double lon2){
+        return 2*EARTH_RADIUS * Math.asin(Math.sqrt(sinsqhd(lat1,lat2) + Math.cos(lat1) * Math.cos(lat2) * sinsqhd(lon1,lon2)));
+    }
+
+    public String info() {
+        return "myPrimes(int,int) - int[]\n" +
+                "calculates the number of primes in the given range (min, max) and the highest of them (returns 0 if none)" +
+                "distance(double,double,double,double) - double\n" +
+                "given the latitudes and longitudes for two points on Earth (lat1,lon1, lat2,lon2) calculates the distance between them\n" +
+                "" +
+                "" +
+                "info() - String" +
+                "returns this text";
+    }
+
     public int execAsy(int x){
         System.out.println("...wywolano asy -odliczam");
         try{
@@ -39,11 +82,15 @@ public class SerwerRPC {
         return Integer.valueOf(x+y);
     }
     
-     public String handleTask(String sign, String status, double priority, int numberOfTask) {
+    public String handleTask(String sign, String status, double priority, int numberOfTask) {
         return  numberOfTask + ". The task: " + sign + "'  has the priority of " + priority + " and " + status;
     }
-    
-    
 
-
+    /**
+     * Calculates square of sine of half of difference between arguments
+     */
+    private double sinsqhd(double v1,double v2){
+        double r = Math.sin((v1-v2)/2);
+        return r*r;
+    }
 }
